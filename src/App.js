a93ea29f7fea991be098be49ef0bc1c537a281c5
Vadik Samitashvili <totalgol2015@gmail.com>
2021-10-03 15:58:52 +0300

    Change App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import './styles/App.css';
+import Stopwatch from "./components/Stopwatch";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Stopwatch/>
     </div>
   );
 }
